5c58560af7a89662e985c810abc34823a2c50d95
Samuel Boyden <sam@stboyden.com>
2025-11-29 10:35:00 +0000

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import './layout.css';
+	import {PUBLIC_CONVEX_URL} from "$env/static/public";
+	import { setupConvex } from 'convex-svelte';
+	import favicon from '$lib/assets/favicon.svg';
+
+	setupConvex(PUBLIC_CONVEX_URL);
+
+ 	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
